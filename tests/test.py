bd81df3647d0c1d2f5634dbbf940ac092c6ce744
sai sathwika <sathwika2310@gmail.com>
2025-07-21 21:42:13 -0500

    Fixed test file and function names

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,13 @@
+# tests/test_somePython.py
+
 from myPackage import somePython
 
-def test_fahrToKelv():
-    assert somePython.fahrToKelv(32) == 273.15, 'incorrect freezing point!'
+def test_freezing_point():
+    assert somePython.fahr_to_kelvin(32) == 273.15
+
+def test_boiling_point():
+    assert round(somePython.fahr_to_kelvin(212), 2) == 373.15
+
+def test_negative_temp():
+    assert round(somePython.fahr_to_kelvin(-40), 2) == 233.15
 
